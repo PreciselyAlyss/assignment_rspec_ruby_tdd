a12b8fd299bea163fe8647b3c3a3ce5d2edd353f
Alyss Noland <alyss.noland@gmail.com>
2017-08-14 00:10:19 -0500

    Stockpicker code complete and test 66% complete

--- lib/stockpicker.rb
+++ lib/stockpicker.rb
@@ -4,29 +4,45 @@
 # 1) find all pairs of eligible days, 2) find the pair of days with the highest profit
 
 class StockPicker
-  attr_reader :prices
+  attr_reader :prices, :min, :max, :days, :len
 
   def initialize(prices)
     @prices = prices
+    @min = @prices[0]
+    @max = @prices[0]
+    @days = [0,1]
+    @len = @prices.length
   end
 
   def picker
-    # 0 to length of prices array converted to array
-    # repeated_permutations determines the number of times to find output
-    # select values to return where finish is greater than start
-    # return max values by positive diff between price[finish] less price[start]
-    ((0...@prices.length).to_a)
-    .repeated_permutation(2)
-    .select {|start, finish| finish > start}
-    .max_by {|start, finish| @prices[finish] - @prices[start]}
+    @prices.each_with_index do |price, i|
+      if(price < @min && (i < @len-2))
+        @min = price
+        @days[0] = i
+        @max = @prices[i+1]
+        @days[1] = i+1
+      end
+
+      if(price > @max && (i < @len-1))
+        @max = price
+        @days[1] = i
+      end
+    end
   end
 
   def profit
-    # stuff here
+    profit = @max - @min
+    if profit > 0 
+      p @days
+      p "The profit from the sale of this stock will be $#{profit}"
+    else
+      raise "Something went terribly wrong"
+    end
   end
 
 end
 
 # Run script
 pick = StockPicker.new([44, 30, 24, 32, 35, 30, 40, 38, 15])
-
+pick.picker
+pick.profit
--- spec/stockpicker_spec.rb
+++ spec/stockpicker_spec.rb
@@ -4,16 +4,23 @@ require 'spec_helper'
 describe StockPicker do
 
   let(:stockpicker) { described_class.new }
-  let(:nine_values) { described_class.new([44, 30, 24, 32, 35, 30, 40, 38, 15]) }
+  let(:nine) { described_class.new([44, 30, 24, 32, 35, 30, 40, 38, 15]) }
+  let(:four) { described_class.new([2, 7, 17, 1])}
 
   describe '#initialize' do
     it 'creates the prices array' do
-      expect(nine_values.prices).to eq([44, 30, 24, 32, 35, 30, 40, 38, 15])
+      expect(nine.prices).to eq([44, 30, 24, 32, 35, 30, 40, 38, 15])
     end
   end
 
   describe '#picker' do
-    it 'says to buy day 0 and sell day 2 from [2, 7, 17, 1]'
+    before do
+      four.picker
+    end
+    
+    it 'says to buy day 0 and sell day 2 from [2, 7, 17, 1]' do
+      expect(four.days).to eq([0,2])
+    end
   end
 
   describe '#profit' do
